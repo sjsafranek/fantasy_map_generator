9eefa851079b5f6978014333cbd2cfd166fa7de4
stefan safranek <stefan@stefan.stefan>
2018-10-11 11:38:49 -0700

    circle gradient

--- perlin_noise.py
+++ perlin_noise.py
@@ -18,12 +18,14 @@ https://github.com/amitp/mapgen2/blob/master/Map.as
 '''
 
 
+import math
 import noise
 import random
 import numpy as np
 from osgeo import ogr
 
 
+# Open file
 shapefile = 'tmp/mesh.shp'
 driver = ogr.GetDriverByName('ESRI Shapefile')
 datasource = driver.Open(shapefile, 1)  # 0 means read-only. 1 means writeable.
@@ -44,16 +46,6 @@ layer.CreateField(classField)
 perlinField = ogr.FieldDefn("perlin", ogr.OFTReal)
 layer.CreateField(perlinField)
 
-scaleField = ogr.FieldDefn("scale", ogr.OFTReal)
-layer.CreateField(scaleField)
-
-# slopeField = ogr.FieldDefn("slope", ogr.OFTReal)
-# layer.CreateField(slopeField)
-
-# elevationField = ogr.FieldDefn("elevation", ogr.OFTReal)
-# layer.CreateField(elevationField)
-#.end
-
 
 # defaults
 # scale: number that determines at what distance to view the noisemap.
@@ -65,11 +57,10 @@ persistence = 0.5
 # lacunarity: number that determines how much detail is added or removed at each octave (adjusts frequency).
 lacunarity = 2.0
 # base = 0
-base = random.randint(0,1000)
+base = random.randint(0,100000)
 
 
-
-print("generating perlin noise")
+print("Perlin Noise")
 shape = ( (180+180)*10, (85+85)*10 )
 world = np.zeros(shape)
 for i in range(shape[0]):
@@ -84,7 +75,94 @@ for i in range(shape[0]):
                                     base=base)
 
 
-print("annotating voronoi polygons")
+
+'''
+
+print("Circular Gradient")
+center_x, center_y = shape[0] // 2, shape[1] // 2
+circle_grad = np.zeros_like(world)
+
+for y in range(world.shape[0]):
+    for x in range(world.shape[1]):
+        distx = abs(x - center_x)
+        disty = abs(y - center_y)
+        dist = math.sqrt(distx*distx + disty*disty)
+        circle_grad[y][x] = dist
+
+# get it between -1 and 1
+max_grad = np.max(circle_grad)
+circle_grad = circle_grad / max_grad
+circle_grad -= 0.5
+circle_grad *= 2.0
+circle_grad = -circle_grad
+
+# shrink gradient
+for y in range(world.shape[0]):
+    for x in range(world.shape[1]):
+        if circle_grad[y][x] > 0:
+            circle_grad[y][x] *= 20
+
+# get it between 0 and 1
+max_grad = np.max(circle_grad)
+circle_grad = circle_grad / max_grad
+
+
+print("Circular Gradient + Noise")
+world_noise = np.zeros_like(world)
+
+for i in range(shape[0]):
+    for j in range(shape[1]):
+        world_noise[i][j] = (world[i][j] * circle_grad[i][j])
+        # if world_noise[i][j] > 0:
+        #     world_noise[i][j] *= 20
+
+# get it between 0 and 1
+max_grad = np.max(world_noise)
+world_noise = world_noise / max_grad
+
+# print("world")
+# print(world)
+# print()
+# print("circle_grad")
+# print(circle_grad)
+# print()
+# print("world_noise")
+# print(world_noise)
+# print()
+'''
+
+# print("numpy gradient")
+
+# print(np.gradient(gradient_world)[0])
+# world_noise = np.gradient(world)[0]
+print("Circular Gradient")
+
+center_x, center_y = shape[0] // 2, shape[1] // 2
+circle_grad = np.zeros_like(world)
+
+for y in range(world.shape[0]):
+    for x in range(world.shape[1]):
+        distx = abs(x - center_x)
+        disty = abs(y - center_y)
+        dist = math.sqrt(distx*distx + disty*disty)
+        circle_grad[y][x] = dist
+
+# get it between -1 and 1
+max_grad = np.max(circle_grad)
+circle_grad = circle_grad / max_grad
+circle_grad -= 0.5
+circle_grad *= 2.0
+circle_grad = -circle_grad
+
+
+print(circle_grad)
+
+exit()
+
+
+
+
+print("Colorized Circular Gradient + Noise")
 count = layer.GetFeatureCount()
 for i in range(count):
     feature = layer.GetFeature(i)
@@ -94,7 +172,10 @@ for i in range(count):
     x = int(lon+180*10)
     lat = point.GetY()
     y = int(lat+85*10)
-    perlin = world[x][y]
+
+    # perlin = world[x][y]
+    perlin = world_noise[x][y]
+
     feature.SetField("perlin", perlin)
     # set land type classification
     if perlin < -0.025:
@@ -116,16 +197,7 @@ for i in range(count):
     #.end
     layer.SetFeature(feature)
 
-'''
-lightblue = [0,191,255]
-blue = [65,105,225]
-green = [34,139,34]
-darkgreen = [0,100,0]
-sandy = [210,180,140]
-beach = [238, 214, 175]
-snow = [255, 250, 250]
-mountain = [139, 137, 137]
-'''
+
 
 
 feature.Destroy()
@@ -133,18 +205,6 @@ feature = None
 
 datasource.Destroy()
 layer = None
-
-
-#     layer.SetFeature(feature)
-#
-#
-# print("classifying voronoi polygons")
-# for i in range(count):
-#     feature = layer.GetFeature(i)
-#     perlin = feature.GetField("perlin")
-#     scale = (perlin-pmin)/(pmax-pmin)
-#     feature.SetField("scale", scale)
-#     feature.SetField("elevation", (scale * perlin))
 
 
 
@@ -158,68 +218,6 @@ sw.AddPoint(-180.0, -85.0)
 ne = ogr.Geometry(ogr.wkbPoint)
 ne.AddPoint(180.0, 85.0)
 max_distance = sw.Distance(ne)
-
-
-def formLandMass(source=None):
-    # random landmass if none supplied
-    island = None
-    if not source:
-        num = random.randint(0, count)
-        island = layer.GetFeature(num)
-    else:
-        island = source
-
-    # get source elevation
-    source_elevation = random.uniform(0.1, 1.0)
-    if island.GetField("elevation"):
-        source_elevation = (island.GetField("elevation") + source_elevation) / 2
-    island.SetField("elevation", source_elevation)
-    layer.SetFeature(island)
-    geomI = island.GetGeometryRef()
-
-    # generate radius of influence
-    radius = random.uniform(
-            max_distance*(source_elevation/24),
-            max_distance*(source_elevation/12)
-        )
-
-    # loop through features
-    for i in range(count):
-        feature = layer.GetFeature(i)
-        geomF = feature.GetGeometryRef()
-        dist = geomI.Distance(geomF)
-        if dist < radius:
-            elev = feature.GetField("elevation")
-            target_elevation = 0
-            if not elev or elev > 0.1:
-                perc = (dist + -0.2)/(radius + -0.2)
-                target_elevation = source_elevation / (1+perc)
-                # randomize
-                perlin = feature.GetField("perlin")
-                scale  = feature.GetField("scale")
-                target_elevation = target_elevation + (scale * perlin)
-                #.end
-            else:
-                target_elevation = (source_elevation + elev)/2
-                if 0 != random.randint(0,3):
-                    target_elevation += random.uniform(-0.1, 0.05)
-            if 1 < target_elevation:
-                target_elevation = 1.0
-            elif 0 > target_elevation:
-                target_elevation = 0.0
-            feature.SetField("elevation", target_elevation)
-            layer.SetFeature(feature)
-            # spawn new
-            if 0  == random.randint(0, 30) and not source:
-                formLandMass(feature)
-
-
-for i in range(30):
-    print("Seed: {0}".format(i))
-    formLandMass()
-
-
-
 
 
 # TODO
